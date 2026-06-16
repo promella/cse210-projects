ae53cd563da1ea51c65a90836e0dac6b5c6267f4
promellawdd130 <pncube8@byupathway.edu>
2026-06-16 22:44:14 +0200

    exercising

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>();
+
+        activities.Add(
+            new Running(
+                new DateTime(2022, 11, 3),
+                30,
+                4.8));
+
+        activities.Add(
+            new Cycling(
+                new DateTime(2022, 11, 4),
+                45,
+                25.0));
+
+        activities.Add(
+            new Swimming(
+                new DateTime(2022, 11, 5),
+                40,
+                40));
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
